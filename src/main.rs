7f11db29dbd56498a81e381e24b710935c8850ed
Bacotch <37607259+Bacotch@users.noreply.github.com.>
2025-08-05 17:13:21 +0900

    feat: Add Linux support and dynamic executable handling
    This commit introduces significant changes to support running the application on Linux, in addition to Windows.
    The main changes are:
    Dynamic executable name: The application now determines the correct default executable name (bedrock_server.exe or bedrock_server) based on the OS.
    Linux-specific environment variable: For Linux, the LD_LIBRARY_PATH environment variable is set to . to ensure the Bedrock Dedicated Server can find its dynamically linked libraries.
    Improved OS handling: The App struct now stores OS information to handle platform-specific configurations correctly.
    
    This ensures the application can be seamlessly built and run on both Windows and Linux without manual configuration changes.

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,14 @@ use std::env;
 use tokio::main;
 #[main]
 async fn main() {
+    let os = env::consts::OS;
     let cwd = env::args().nth(1).unwrap_or(".".to_string());
-    let executable_name = env::args()
-        .nth(2)
-        .unwrap_or("bedrock_server.exe".to_string());
-    let app = App::new(cwd, executable_name);
+      let default_executable_name = match os {
+        "windows" => "bedrock_server.exe",
+        "linux" => "bedrock_server",
+        _ => panic!("Unsupported operating system: {}", os)
+    };
+    let executable_name = env::args().nth(2).unwrap_or(default_executable_name.to_string());
+    let app = App::new(cwd, executable_name,os.to_string());
     app.run().await
 }
